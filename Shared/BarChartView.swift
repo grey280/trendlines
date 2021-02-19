a341f6ea97c2ff73cd4ab776d79c3f25fc606307
grey280 <greypatterson@gmail.com>
2021-02-19 12:33:01 -0800

    generics!

--- Shared/BarChartView.swift
+++ Shared/BarChartView.swift
@@ -7,14 +7,38 @@
 
 import SwiftUI
 
-struct BarChartView: View {
+struct BarChartView<X: Comparable, Y: Numeric>: View {
+    struct DataPoint<X: Comparable, Y: Numeric> {
+        let x: X
+        let y: Y
+    }
+    
+    let data: [DataPoint<X,Y>]
+    
+    private var xRange: (X, X)? {
+        guard data.count > 0 else {
+            return nil
+        }
+        let sorted = data.map { $0.x }.sorted()
+        return (sorted.first!, sorted.last ?? sorted.first!)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            // todo: y axis
+            
+        }
     }
 }
 
 struct BarChartView_Previews: PreviewProvider {
+    static let testData: [BarChartView<Int, Int>.DataPoint<Int, Int>] = [
+        .init(x: 1, y: 1),
+        .init(x: 2, y: 2),
+        .init(x: 3, y: 3)
+    ]
+    
     static var previews: some View {
-        BarChartView()
+        BarChartView<Int,Int>(data: testData)
     }
 }
