11470be342910285b2dcef8aa648f491f0855322
grey280 <greypatterson@gmail.com>
2021-02-19 12:22:01 -0800

    Initial Commit

---
+++ Shared/trendlinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  trendlinesApp.swift
+//  Shared
+//
+//  Created by Grey Patterson on 2/19/21.
+//
+
+import SwiftUI
+
+@main
+struct trendlinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
