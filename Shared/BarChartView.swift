4a70e96e86495859ab625e8afa84c3e62341bd2c
grey280 <greypatterson@gmail.com>
2021-02-19 12:32:55 -0800

    start on bar chart view

---
+++ Shared/BarChartView.swift
@@ -0,0 +1,20 @@
+//
+//  BarChartView.swift
+//  trendlines
+//
+//  Created by Grey Patterson on 2/19/21.
+//
+
+import SwiftUI
+
+struct BarChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BarChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        BarChartView()
+    }
+}
