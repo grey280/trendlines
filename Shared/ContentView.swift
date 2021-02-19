9dba66473ceb89f295f585ee9ce1359e06f745bb
grey280 <greypatterson@gmail.com>
2021-02-19 15:55:41 -0800

    overlay test

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            RangedBarChartView<Int>(data: (0...30).map { RangedBarChartView<Int>.DataPoint(x: $0, yMin: Double($0), yMax: Double($0 + 2) )}, unit: "Stuff", color: .red, hasOverlay: true)
+            BarChartView<Int>(data: (0...30).map { BarChartView<Int>.DataPoint(x: 30 - $0, y: Double($0) )}, unit: "Things", color: .blue, axisAlignment: .trailing, hasOverlay: true)
+        }
     }
 }
 
