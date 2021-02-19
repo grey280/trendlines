52c40cd8a63954e9f06b9154037e6e0c0e051c66
grey280 <greypatterson@gmail.com>
2021-02-19 14:05:53 -0800

    organize

--- Shared/BarChartView.swift
+++ Shared/BarChartView.swift
@@ -7,37 +7,6 @@
 
 import SwiftUI
 
-struct YAxisView: View {
-    enum AxisAlignment {
-        case leading, trailing
-    }
-    
-    public static let width: CGFloat = 30
-    
-    public init(min: String, max: String, unit: String, color: Color = .gray) {
-        self.min = min
-        self.max = max
-        self.unit = unit
-        self.color = color
-    }
-    
-    let min: String
-    let max: String
-    let unit: String
-    let color: Color
-    
-    var body: some View {
-        VStack(alignment: .center) {
-            Text(max)
-            Spacer()
-            Text(unit).rotationEffect(.degrees(-90)).fixedSize()
-            Spacer()
-            Text(min)
-        }.foregroundColor(color).font(.footnote)
-        .frame(width: YAxisView.width)
-    }
-}
-
 struct BarChartView<X: Hashable & Comparable>: View {
     struct DataPoint {
         let x: X
---
+++ Shared/YAxisView.swift
@@ -0,0 +1,45 @@
+//
+//  YAxisView.swift
+//  trendlines
+//
+//  Created by Grey Patterson on 2/19/21.
+//
+
+import SwiftUI
+
+struct YAxisView: View {
+    enum AxisAlignment {
+        case leading, trailing
+    }
+    
+    public static let width: CGFloat = 30
+    
+    public init(min: String, max: String, unit: String, color: Color = .gray) {
+        self.min = min
+        self.max = max
+        self.unit = unit
+        self.color = color
+    }
+    
+    let min: String
+    let max: String
+    let unit: String
+    let color: Color
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            Text(max)
+            Spacer()
+            Text(unit).rotationEffect(.degrees(-90)).fixedSize()
+            Spacer()
+            Text(min)
+        }.foregroundColor(color).font(.footnote)
+        .frame(width: YAxisView.width)
+    }
+}
+
+struct YAxisView_Previews: PreviewProvider {
+    static var previews: some View {
+        YAxisView(min: "0", max: "10", unit: "Things")
+    }
+}
