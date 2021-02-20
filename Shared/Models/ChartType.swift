4ca106886b9fde88f5d8404d5277d4e2136b0b99
grey280 <greypatterson@gmail.com>
2021-02-20 10:24:02 -0800

    chart type

---
+++ Shared/Models/ChartType.swift
@@ -0,0 +1,12 @@
+//
+//  ChartType.swift
+//  trendlines
+//
+//  Created by Grey Patterson on 2/20/21.
+//
+
+import Foundation
+
+enum ChartType: String, Codable {
+    case bar, floatingBar, line
+}
