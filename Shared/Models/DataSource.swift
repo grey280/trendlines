33f23be60c2610958e224bc6ab763c8a9a656db8
grey280 <greypatterson@gmail.com>
2021-02-20 10:58:57 -0800

    encoding?

--- Shared/Models/DataSource.swift
+++ Shared/Models/DataSource.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import GRDB
 
 struct DataSource {
     typealias ID = Int64
@@ -15,4 +16,25 @@ struct DataSource {
     var title: String?
     var color: Color
     var chartType: ChartType?
+}
+
+extension DataSource: MutablePersistableRecord {
+    func encode(to container: inout PersistenceContainer) {
+        container["id"] = id
+        let JSONencoder = JSONEncoder()
+        if let encoded = try? JSONencoder.encode(sourceType) {
+            container["sourceType"] = encoded
+        }
+        container["title"] = title
+        if let encColor = try? JSONencoder.encode(color) {
+            container["color"] = encColor
+        }
+        if let chartType = chartType, let encType = try? JSONencoder.encode(chartType) {
+            container["chartType"] = encType
+        }
+    }
+    
+    mutating func didInsert(with rowID: Int64, for column: String?) {
+        id = rowID
+    }
 }
