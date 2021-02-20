2fc48c51e263f8901a97452fab5d443944152fab
grey280 <greypatterson@gmail.com>
2021-02-19 18:01:00 -0800

    More test stuff

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,16 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    let r: Bool
+    
     var body: some View {
         ZStack {
             RangedBarChartView<Int>(data: (0...30).map { RangedBarChartView<Int>.DataPoint(x: $0, yMin: Double($0), yMax: Double($0 + 2) )}, unit: "Stuff", color: .red, hasOverlay: true)
-            BarChartView<Int>(data: (0...30).map { BarChartView<Int>.DataPoint(x: 30 - $0, y: Double($0) )}, unit: "Things", color: .blue, axisAlignment: .trailing, hasOverlay: true)
+            if r {
+                BarChartView<Int>(data: (0...30).map { BarChartView<Int>.DataPoint(x: 30 - $0, y: Double($0) )}, unit: "Things", color: .blue, axisAlignment: .trailing, hasOverlay: true)
+            } else {
+                LineChartView<Int>(data: (0...30).map { LineChartView<Int>.DataPoint(x: $0, y: Double.random(in: 0..<30.0) )}, unit: "Things", color: .blue, axisAlignment: .trailing, hasOverlay: true)
+            }
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView(r: true)
+            ContentView(r: false)
+        }
     }
 }
--- Shared/trendlinesApp.swift
+++ Shared/trendlinesApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct trendlinesApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(r: false)
         }
     }
 }
