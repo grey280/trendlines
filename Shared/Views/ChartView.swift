0a8c69f7c160a6a04fbb37ee72a9c0dcdcd747bc
grey280 <greypatterson@gmail.com>
2021-02-20 12:42:46 -0800

    something to handle logic for building charts

---
+++ Shared/Views/ChartView.swift
@@ -0,0 +1,20 @@
+//
+//  ChartView.swift
+//  trendlines
+//
+//  Created by Grey Patterson on 2/20/21.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartView()
+    }
+}
