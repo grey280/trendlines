8dbc102b28dc197186e358cc2914a95dec70097c
grey280 <greypatterson@gmail.com>
2021-02-20 11:07:00 -0800

    cleanup

--- Shared/Models/DataSource.swift
+++ Shared/Models/DataSource.swift
@@ -6,25 +6,11 @@
 //
 
 import SwiftUI
-//import GRDB
 
 struct DataSource {
-//    typealias ID = Int64
-//    var id: ID?
-    
     var sourceType: DataSourceType
     var title: String?
     var color: Color
     var chartType: ChartType?
 }
 extension DataSource: Codable { }
-
-//
-//extension DataSource: MutablePersistableRecord {
-//    mutating func didInsert(with rowID: Int64, for column: String?) {
-//        id = rowID
-//    }
-//}
-//
-//extension DataSource: FetchableRecord { }
-//extension DataSource: TableRecord { }
