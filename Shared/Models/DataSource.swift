9502fe3204f1ac63782cf89f8db142739bd46b0e
grey280 <greypatterson@gmail.com>
2021-02-20 13:19:28 -0800

    stub unit name loader

--- Shared/Models/DataSource.swift
+++ Shared/Models/DataSource.swift
@@ -36,4 +36,40 @@ extension DataSource {
             }
         }
     }
+    
+    var unitName: String {
+        switch sourceType {
+        case .entries:
+            return "count"
+        case .health(let healthSubtype):
+            switch healthSubtype {
+            case .activity(let activityType):
+                switch activityType {
+                case .activeEnergy:
+                    return "Calories" // TODO: Replace with unit lookup
+                case .cyclingDistance, .walkRunDistance:
+                    return "miles" // TODO: Replace with unit lookup
+                case .flightsClimbed:
+                    return "flights"
+                case .mindfulMinutes, .workoutTime:
+                    return "minutes"
+                case .sleep, .standHours:
+                    return "hours"
+                case .steps:
+                    return "steps"
+                case .swimDistance:
+                    return "yards" // TODO: Replace with unit lookup
+                }
+            case .nutrition:
+                return .bar
+            case .body(let bodySubtype):
+                switch bodySubtype {
+                case .restingHeartRate, .heartRateVariability, .bodyWeight, .leanBodyMass, .bodyFatPercentage:
+                    return .line
+                case .heartRate, .bloodPressure:
+                    return .floatingBar
+                }
+            }
+        }
+    }
 }
