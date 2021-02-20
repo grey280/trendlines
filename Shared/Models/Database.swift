1a9693cf0927213e028e8a86b52fdac171dae569
grey280 <greypatterson@gmail.com>
2021-02-20 12:05:00 -0800

    database stub

---
+++ Shared/Models/Database.swift
@@ -0,0 +1,14 @@
+//
+//  Database.swift
+//  trendlines
+//
+//  Created by Grey Patterson on 2/20/21.
+//
+
+import Combine
+import Foundation
+import GRDB
+
+class Database: ObservableObject {
+    
+}
