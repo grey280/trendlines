d578f64f882aeb0fe03f7d94d68f7f8c163baf31
grey280 <greypatterson@gmail.com>
2021-02-20 12:13:19 -0800

    create connection, do some logging

--- Shared/Models/Database.swift
+++ Shared/Models/Database.swift
@@ -7,8 +7,24 @@
 
 import Combine
 import Foundation
+import OSLog
 import GRDB
 
 class Database: ObservableObject {
+    let logger: Logger
+    let dbQueue: DatabaseQueue
     
+    init?() {
+        logger = Logger(subsystem: "net.twoeighty.trendlines", category: "Database")
+        guard let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            logger.error("Could not get document directory URL")
+            return nil
+        }
+        do {
+            dbQueue = try DatabaseQueue(path: url.absoluteString)
+        } catch {
+            logger.error("Could not create database: \(error.localizedDescription, privacy: .public)")
+            return nil
+        }
+    }
 }
