aa2f881e686db24f7da6a1d1e9cc855112f4a7d2
grey280 <greypatterson@gmail.com>
2021-02-21 09:22:45 -0800

    Two different things

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -10,11 +10,21 @@ import SwiftUI
 struct ContentView: View {
     @EnvironmentObject var database: Database
     
+    @State var showingAdd = false
+    
     var body: some View {
         NavigationView {
             List(database.charts, id: \.id) {
                 ChartView(chart: $0)
             }
+                .navigationTitle("Dashboard")
+            .navigationBarItems(leading: Button(action: {
+                showingAdd.toggle()
+            }, label: {
+                Image(systemName: "plus")
+            }), trailing: NavigationLink(destination: Text("Settings!"), label: {
+                Image(systemName: "gear")
+            }))
         }
     }
 }
