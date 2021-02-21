1d17a8f9e26ddb9c8b366714b80d2d8cf19cb3a0
grey280 <greypatterson@gmail.com>
2021-02-21 15:59:01 -0800

    visual tweaks

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List(database.charts, id: \.id) {
-                ChartView(chart: $0)
+                ChartView(chart: $0).frame(minHeight: 150).padding()
             }
                 .navigationTitle("Dashboard")
             .navigationBarItems(leading: Button(action: {
