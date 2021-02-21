5c9d6aefd161a9d5ce5f5fb0da71c62ca751be15
grey280 <greypatterson@gmail.com>
2021-02-21 10:23:01 -0800

    start on picker helper

--- Shared/Views/ChartBuilderView.swift
+++ Shared/Views/ChartBuilderView.swift
@@ -45,6 +45,36 @@ struct ChartBuilderView: View {
     }
 }
 
+struct SourceTypePicker: View {
+    @Binding var sourceType: DataSourceType
+    
+    var body: some View {
+        Picker("Data Type", selection: $sourceType) {
+            Section(header: Text("Custom")) {
+                Text("Data Set").tag(DataSourceType.entries)
+            }
+            Section(header: Text("Body")) {
+                Text("Resting Heart Rate").tag(DataSourceType.health(.body(.restingHeartRate)))
+                Text("Heart Rate Variability").tag(DataSourceType.health(.body(.heartRateVariability)))
+                Text("Heart Rate").tag(DataSourceType.health(.body(.heartRate)))
+                Text("Body Mass").tag(DataSourceType.health(.body(.bodyWeight)))
+                Text("Lean Body Mass").tag(DataSourceType.health(.body(.leanBodyMass)))
+                Text("Body Fat Percentage").tag(DataSourceType.health(.body(.bodyFatPercentage)))
+            }
+            Section(header: Text("Activity")) {
+                Text("Calories Burned").tag(DataSourceType.health(.activity(.activeEnergy)))
+                Text("Walk/Run Distance").tag(DataSourceType.health(.activity(.walkRunDistance)))
+                Text("Swim Distance").tag(DataSourceType.health(.activity(.swimDistance)))
+                Text("Cycling Distance").tag(DataSourceType.health(.activity(.cyclingDistance)))
+                Text("Flights Climbed").tag(DataSourceType.health(.activity(.flightsClimbed)))
+                Text("Steps").tag(DataSourceType.health(.activity(.steps)))
+                Text("Stand Hours").tag(DataSourceType.health(.activity(.standHours)))
+                Text("Exercise Time").tag(DataSourceType.health(.activity(.workoutTime)))
+            }
+        }
+    }
+}
+
 struct ChartBuilderView_Previews: PreviewProvider {
     static var previews: some View {
         ChartBuilderView() { _ in }
