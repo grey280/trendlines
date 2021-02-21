728ac9883c64b072310ff4957a2c275d5563c0cc
grey280 <greypatterson@gmail.com>
2021-02-21 09:53:13 -0800

    reusable!

--- Shared/Views/ChartBuilderView.swift
+++ Shared/Views/ChartBuilderView.swift
@@ -9,12 +9,23 @@ import SwiftUI
 
 struct ChartBuilderView: View {
     
+    init(_ onSave: @escaping (Chart) -> Void) {
+        self.onSave = onSave
+        self._chart = State(initialValue: Chart(id: nil, sortNo: 1, source1: DataSource(sourceType: .entries, title: nil, color: .gray, chartType: nil), source2: nil))
+    }
+    init(chart: Chart, _ onSave: @escaping (Chart) -> Void) {
+        self.onSave = onSave
+        self._chart = State(initialValue: chart)
+    }
+    
     let onSave: (Chart) -> Void
     
-    @State var chart = Chart(id: nil, sortNo: 1, source1: DataSource(sourceType: .entries, title: nil, color: .gray, chartType: nil), source2: nil)
+    @State var chart: Chart
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            
+        }
     }
 }
 
