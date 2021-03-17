4d622ad39de2a3f328d7195ca0a14fdcb9120f47
grey280 <greypatterson@gmail.com>
2021-03-16 19:27:15 -0700

    Basic layout, delete

--- Shared/SettingsView.swift
+++ Shared/SettingsView.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @EnvironmentObject var database: Database
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                ForEach(database.charts, id: \.id) { chart in
+                    if let source2 = chart.source2 {
+                        HStack(spacing: 2) {
+                            ChartTitleView(source: chart.source1)
+                            Text(" and ")
+                            ChartTitleView(source: source2)
+                        }
+                    } else {
+                        HStack {
+                            ChartTitleView(source: chart.source1)
+                        }
+                    }
+                }
+                .onDelete(perform: onDelete)
+                .onMove(perform: onMove)
+            }
+        }
+    }
+    
+    private func onDelete(offsets: IndexSet) {
+        let charts = offsets.map {
+            database.charts[$0]
+        }
+        for chart in charts {
+            do {
+                try database.remove(chart: chart)
+            } catch {
+                #warning("Missing error handling.")
+            }
+        }
+    }
+    
+    private func onMove(source: IndexSet, destination: Int) {
+        
     }
 }
 
