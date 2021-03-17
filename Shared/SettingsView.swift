7a7c1a082a4841cf95e382365b88f2d1841c5b8a
grey280 <greypatterson@gmail.com>
2021-03-16 19:19:52 -0700

    Add Settings view

---
+++ Shared/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  trendlines
+//
+//  Created by Grey Patterson on 3/16/21.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
