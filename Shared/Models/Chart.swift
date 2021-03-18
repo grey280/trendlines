6e8ba4ddfe0c95e012a57cc9a19b847c9fa984ce
grey280 <greypatterson@gmail.com>
2021-03-17 20:35:20 -0700

    switch to custom encoding

--- Shared/Models/Chart.swift
+++ Shared/Models/Chart.swift
@@ -15,14 +15,29 @@ struct Chart {
     var sortNo: Int64
     var source1: DataSource
     var source2: DataSource?
+    
+    enum Columns: String, ColumnExpression {
+        case id, sortNo
+        case dataSource1Type, dataSource1DataSet, dataSource1Title, dataSource1Color, dataSource1ChartType
+        case dataSource2Type, dataSource2DataSet, dataSource2Title, dataSource2Color, dataSource2ChartType
+    }
 }
 
-extension Chart: Codable { }
+//extension Chart: Codable { }
 extension Chart: MutablePersistableRecord {
     mutating func didInsert(with rowID: Int64, for column: String?) {
         id = rowID
     }
 }
-extension Chart: FetchableRecord { }
+extension Chart: FetchableRecord {
+    init(row: Row) {
+        id = row[Columns.id]
+        sortNo = row[Columns.sortNo]
+        // unpack dataSource1
+        
+        // unpack dataSource2
+        
+    }
+}
 extension Chart: TableRecord { }
 extension Chart: Identifiable { }
