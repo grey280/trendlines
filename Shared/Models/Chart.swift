59b52fa9e5890a49a15efbccf6bd6748473be67a
grey280 <greypatterson@gmail.com>
2021-03-17 20:47:34 -0700

    start unpacking

--- Shared/Models/Chart.swift
+++ Shared/Models/Chart.swift
@@ -5,7 +5,7 @@
 //  Created by Grey Patterson on 2/20/21.
 //
 
-import Foundation
+import SwiftUI
 import GRDB
 
 struct Chart {
@@ -34,10 +34,21 @@ extension Chart: FetchableRecord {
         id = row[Columns.id]
         sortNo = row[Columns.sortNo]
         // unpack dataSource1
-        
+        #warning("Replace with DataSet.ID type reference")
+        let source1Reference: Int64? = row[Columns.dataSource1DataSet]
+        var source1Type: DataSourceType = row[Columns.dataSource1Type]
+        if let s1r = source1Reference {
+            source1Type = .entries(datasetID: s1r) // because the source1Reference has a FK, we know from the db that it's present
+        }
+        let source1Title: String = row[Columns.dataSource1Title]
+        let source1Color: Color = row[Columns.dataSource1Color]
+        let source1Chart: ChartType = row[Columns.dataSource1ChartType]
+        source1 = DataSource(sourceType: source1Type, title: source1Title, color: source1Color, chartType: source1Chart)
         // unpack dataSource2
         
     }
 }
 extension Chart: TableRecord { }
 extension Chart: Identifiable { }
+
+extension Color: DatabaseValueConvertible {}
