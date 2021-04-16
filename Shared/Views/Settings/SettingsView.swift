ed2cf42fe257a781390e72f85002f74375ee91b8
grey280 <greypatterson@gmail.com>
2021-04-15 19:07:56 -0700

    Another fix to preview

--- Shared/Views/Settings/SettingsView.swift
+++ Shared/Views/Settings/SettingsView.swift
@@ -107,7 +107,9 @@ struct SettingsView: View {
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
         if let db = Database() {
-            SettingsView().environmentObject(db)
+            NavigationView {
+                SettingsView().environmentObject(db)
+            }
         } else {
             EmptyView()
         }
