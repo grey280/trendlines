944b91626d32ca7fd51194582b1e1170c42b1267
grey280 <greypatterson@gmail.com>
2021-04-15 17:51:41 -0700

    Use it to reload

--- Shared/Controllers/DatabaseDataProvider.swift
+++ Shared/Controllers/DatabaseDataProvider.swift
@@ -6,25 +6,32 @@
 //
 
 import Foundation
+import Combine
 import OSLog
 
 class DatabaseProvider: DataProvider {
     let dataSet: DataSet
     let database: Database
     let mode: DataSourceDisplayMode
+    private var updateSubscription: AnyCancellable?
     
     private let logger = Logger(subsystem: "net.twoeighty.trendlines", category: "DatabaseProvider")
+    
     
     init?(dataSet: DataSet, database: Database, mode: DataSourceDisplayMode) {
         self.dataSet = dataSet
         self.database = database
         self.mode = mode
-        
         super.init()
         loadData()
     }
     
     func loadData() {
+        if updateSubscription == nil {
+            self.updateSubscription = database.datasetUpdatedPublisher.sink { (_) in
+                self.loadData()
+            }
+        }
         guard let startDate = Calendar.current.date(byAdding: .day, value: -30, to: Date()) else {
             logger.error("Could not create '30 days ago' date.")
             return
