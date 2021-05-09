993aec23ebe0495a2a94e3ed2b60fe5f4c0c5490
grey280 <greypatterson@gmail.com>
2021-05-08 20:50:47 -0700

    Start stubbing the file model

--- Shared/Models/DataSetExport.swift
+++ Shared/Models/DataSetExport.swift
@@ -5,4 +5,36 @@
 //  Created by Grey Patterson on 5/8/21.
 //
 
-import Foundation
+import SwiftUI
+import UniformTypeIdentifiers
+
+struct DataSetExport: FileDocument {
+    static var readableContentTypes: [UTType] {
+        [UTType.commaSeparatedText]
+    }
+    
+    var entries: [DataSetEntry]
+    
+    init(entries: [DataSetEntry]) {
+        self.entries = entries
+    }
+    
+    init(configuration: ReadConfiguration) throws {
+        <#code#>
+    }
+    
+    func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
+        <#code#>
+    }
+    
+    static func convertToCSVLine(entry: DataSetEntry) -> String {
+        "\(entry.value),\(entry.dateAdded)"
+    }
+    
+    func createCSV() -> String {
+        var result = "Value,Date\n"
+        let converted = entries.map(DataSetExport.convertToCSVLine)
+        result += converted.joined(separator: "\n")
+        return result
+    }
+}
